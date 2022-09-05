c14047d73ad42b798edcc2b884bf3c6ac11714f3
SeungHeon <84365977+song1248@users.noreply.github.com>
2022-09-05 15:22:28 +0900

    Update code.py

--- Leetcode/1008. Construct Binary Search Tree from Preorder Traversal/code.py
+++ Leetcode/1008. Construct Binary Search Tree from Preorder Traversal/code.py
@@ -10,7 +10,8 @@ class Solution:
         if len(preorder) == 0:
             return None
         
-        root = TreeNode(preorder[0])
+        root = TreeNode()]
+        root.val = preorder[0]
         root.left = self.bstFromPreorder([i for i in preorder if i<root.val])
         root.right = self.bstFromPreorder([i for i in preorder if i>root.val])
         
